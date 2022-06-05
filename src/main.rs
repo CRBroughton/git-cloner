605899d9ced7b00d763e7ebb5a29bf4343687a00
CRBroughton <crbroughton@posteo.uk>
2022-06-05 11:26:42 +0100

    formatted file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{process::Command as OSCommand};
-use clap::{Command, arg};
+use clap::{arg, Command};
+use std::process::Command as OSCommand;
 
 fn main() {
     let matches = Command::new("Git Cloner")
@@ -7,25 +7,26 @@ fn main() {
         .author("Craig R Broughton <CRBroughton@posteo.uk")
         .about("Clones git repositories")
         .arg_required_else_help(true)
-        .subcommand(Command::new("repo")
-            .about("Your chosen repository")
-            .arg(arg!([REPO_PATH])))
-        .subcommand(Command::new("init")
-            .about("Initialises a git repository"))
+        .subcommand(
+            Command::new("repo")
+                .about("Your chosen repository")
+                .arg(arg!([REPO_PATH])),
+        )
+        .subcommand(Command::new("init").about("Initialises a git repository"))
         .get_matches();
 
-        match matches.subcommand() {
-            Some(("init", _sub_matches)) => init_repo(),
-            Some(("repo", sub_matches)) => 
-                if sub_matches.value_of("REPO_PATH").is_some() {
-                    clone_repo(sub_matches.value_of("REPO_PATH").unwrap().to_string())
-                } else {
-                    println!("You failed to provide a valid path...")
-                }
-            _ => println!("You failed to enter any known command..."),
+    match matches.subcommand() {
+        Some(("init", _sub_matches)) => init_repo(),
+        Some(("repo", sub_matches)) => {
+            if sub_matches.value_of("REPO_PATH").is_some() {
+                clone_repo(sub_matches.value_of("REPO_PATH").unwrap().to_string())
+            } else {
+                println!("You failed to provide a valid path...")
+            }
         }
+        _ => println!("You failed to enter any known command..."),
+    }
 }
-
 
 fn init_repo() {
     let output = OSCommand::new("git")
@@ -33,15 +34,15 @@ fn init_repo() {
         .output()
         .expect("failed to initialised repository");
 
-        println!("{:?}", String::from_utf8_lossy(&output.stdout).trim_end());
+    println!("{:?}", String::from_utf8_lossy(&output.stdout).trim_end());
 }
 
 fn clone_repo(repo_link: String) {
     let output = OSCommand::new("git")
-    .arg("clone")
-    .arg(repo_link)
-    .output()
-    .expect("failed to initialised repository");
+        .arg("clone")
+        .arg(repo_link)
+        .output()
+        .expect("failed to initialised repository");
 
     println!("{:?}", String::from_utf8_lossy(&output.stderr).trim_end());
 }
