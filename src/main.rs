b58366a5629f65abdf245c3a000f6827d1473d45
CRBroughton <crbroughton@posteo.uk>
2022-06-05 11:10:29 +0100

    refactored to use builder api

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,42 @@
-use clap::Parser;
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[clap(name = "Git Cloner")]
-#[clap(author = "Craig R Broughton <crbroughton@posteo.uk>")]
-#[clap(version = "0.0.1")]
-#[clap(about = "Clones git repositories", long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[clap(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[clap(short, long, default_value_t = 1)]
-    count: u8,
-}
+use std::{process::Command as OSCommand};
+use clap::{Command, arg};
 
 fn main() {
-    let args = Args::parse();
+    let matches = Command::new("Git Cloner")
+        .version("0.0.1")
+        .author("Craig R Broughton <CRBroughton@posteo.uk")
+        .about("Clones git repositories")
+        .arg_required_else_help(true)
+        .subcommand(Command::new("repo")
+            .about("Your chosen repository")
+            .arg(arg!([REPO_PATH])))
+        .subcommand(Command::new("init")
+            .about("Initialises a git repository"))
+        .get_matches();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+        match matches.subcommand() {
+            Some(("init", _sub_matches)) => init_repo(),
+            Some(("repo", sub_matches)) => clone_repo(sub_matches.value_of("REPO_PATH").unwrap().to_string()),
+            _ => println!("You failed to enter any known command..."),
+        }
+}
+
+
+fn init_repo() {
+    let output = OSCommand::new("git")
+        .arg("init")
+        .output()
+        .expect("failed to initialised repository");
+
+        println!("{:?}", String::from_utf8_lossy(&output.stderr).trim_end());
+}
+
+fn clone_repo(repo_link: String) {
+    let output = OSCommand::new("git")
+    .arg("clone")
+    .arg(repo_link)
+    .output()
+    .expect("failed to initialised repository");
+
+    println!("{:?}", String::from_utf8_lossy(&output.stderr).trim_end());
 }
